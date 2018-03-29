d1aba77985452a31fbe1dea51e25cd6b10c84efa
Steven1230 <yxb841230@126.com>
2018-03-29 16:59:29 +0800

    initially

---
+++ src/main/java/main/java/com/fico/testCaseGenerator/converter/TargetBOMBeanConverMapper.java
@@ -0,0 +1,31 @@
+package com.fico.testCaseGenerator.converter;
+
+import javax.xml.transform.Source;
+import java.util.ResourceBundle;
+
+/**
+ * @Author XiangbinYuan stevenYuan
+ * @CreationDate 3/28/2018
+ */
+public class TargetBOMBeanConverMapper extends BOMBeanConvertMapper {
+
+    private static String packageName = ResourceBundle.getBundle("javaBOM").getString("convertClassPackageName") + ".";
+
+    public TargetBOMBeanConverMapper(String clsSimpleName){
+
+        String javaClassName = packageName + clsSimpleName;
+
+        try {
+            this.setCorrespondingClass( Class.forName(javaClassName  ) );
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private SourceBOMBeanConvertMapper correspondingScourceBOMBeanConvertMapper = null;
+
+
+    public Object newInstance() throws Exception{
+        return this.getCorrespondingClass().newInstance();
+    }
+}
---
+++ src/main/java/main/java/com/fico/testCaseGenerator/util/TestCaseUtils.java
@@ -0,0 +1,18 @@
+package com.fico.testCaseGenerator.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @Author XiangbinYuan stevenYuan
+ * @CreationDate 11/18/2017
+ */
+public class TestCaseUtils {
+
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+
+}
